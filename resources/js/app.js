fc9e9c2c4c67d1c243144751dce81c5f0142952d
babacarmbaye2400@gmail.com <babacarmbaye2400@gmail.com>
2024-06-13 18:46:30 +0000

    Grand projet

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,55 @@
 import './bootstrap';
+
+
+function list_details_Manage(){
+    let show_groups = document.querySelector('#show-groups');
+    let groupes =document.querySelector('.groupes');
+
+    let show_members =document.querySelector('#show-members');
+    let members = document.querySelector('.membres');
+
+    show_groups.addEventListener('click', ()=>{
+        event.preventDefault();
+        members.classList.remove('showed-members')
+        groupes.classList.toggle('showed-groups');
+        show_groups.classList.add('active')
+        show_members.classList.remove('active')
+    })
+
+    
+
+    show_members.addEventListener('click', ()=>{
+        event.preventDefault();
+        groupes.classList.remove('showed-groups');
+        members.classList.toggle('showed-members')
+        show_members.classList.add('active')
+        show_groups.classList.remove('active')
+    })
+}
+
+
+function list_make_manager(){
+    let type =document.querySelector('.saisir-list');
+    let down =document.querySelector('.telecharger-list');
+
+    let list_download =document.querySelector('.list-down');
+    let list_type =document.querySelector('.list-type');
+
+    type.addEventListener('click', ()=>{
+        list_download.classList.toggle('desactive-down');
+        list_type.classList.toggle('active-type');
+        type.classList.toggle('disactive-type-btn');
+        down.classList.toggle('active-down-btn');
+
+    })
+
+    down.addEventListener('click', ()=>{
+        list_download.classList.remove('desactive-down');
+        list_type.classList.remove('active-type');
+        type.classList.remove('disactive-type-btn');
+        down.classList.remove('active-down-btn');
+    })
+}
+
+list_details_Manage();
+list_make_manager();
